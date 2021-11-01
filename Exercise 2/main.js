fd9699a94f92f478687e960e3607bf3b59e16298
Umang <umangrajkarnikar@gmail.com>
2021-11-01 16:21:39 -0400

    Modified the textarea styling in styles.css.

--- Exercise 2/main.js
+++
@@ -1,5 +0,0 @@
-const myContent = document.querySelector('.content');
-myContent.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('.container').style.height = '300pt';
-});
